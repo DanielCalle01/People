cd5dcf069d72568ab4b440516d1750dc91d23e82
DanielCalle01 <a2019211611@estudiantes.upsa.edu.bo>
2021-10-11 22:20:09 -0400

    Agregar archivos de proyecto.

---
+++ WebPeople/Models/Person.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebPeople.Models
+{
+    //Menú de SexType, para que escoja (1)
+    public enum SexType
+    {
+        Female = 0,
+        Male = 1,
+    }
+    public class Person
+    {
+        [Key] //Para validar nuestro modelo tenemos que activar el [key], su libreria
+        public int PersonId { get; set; }
+        
+        [Required(ErrorMessage = "El campo del nombre es requerido")]
+        [StringLength(60, MinimumLength = 5, ErrorMessage = "El nombre tiene que tener entre 5 a 60 caracteres")]
+        [Display(Name = "Nombre completo")]
+        public string Name { get; set; }
+        
+        [Required]
+        [Display(Name = "Sexo")]
+        public SexType Sex { get; set; }
+        
+        [Display(Name = "Pasatiempos")]
+        public string Hobby { get; set; }
+    }
+}
